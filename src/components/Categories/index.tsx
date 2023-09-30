26a69dd79f0aba3891aa8a887c6ca3c6df9d24c3
Renan Holler <rrholler@gmail.com>
2023-09-30 18:36:15 -0300

    Removido espaço desnecessário

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -23,11 +23,10 @@ export function Categories() {
       keyExtractor={category => category._id}
       renderItem={({ item: category }) => {
         const isSelected = selectedCategory === category._id;
-
         return (
           <Category onPress={() => handleSelectCategory(category._id)}>
             <Icon>
-              <Text  opacity={isSelected ? 1 : 0.5}> {category.icon}</Text>
+              <Text opacity={isSelected ? 1 : 0.5}> {category.icon}</Text>
             </Icon>
 
             <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5} >{category.name}</Text>
